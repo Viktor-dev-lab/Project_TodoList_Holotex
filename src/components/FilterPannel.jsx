4b2cfba1012722b43b81047a6e4ef5c42a2e3ed1
dinh-xuan-vns <xuanyeumy123@gmail.com>
2025-09-10 21:55:03 +0700

    fix(filter): resolve selected state not updating

--- src/components/FilterPannel.jsx
+++ src/components/FilterPannel.jsx
@@ -1,4 +1,5 @@
 import "./FilterPannel.css";
+import {useState} from 'react'
 
 const FILTER_ITEMS = [
   {
@@ -24,13 +25,17 @@ const FILTER_ITEMS = [
 ];
 
 const FilterPannel = () => {
+  const [selectFilter, setSelectFilter] = useState("all");
+
   return (
     <div className="filter-pannel">
       <input type="text" name="search-text" placeholder="Search"></input>
       <div className="filter-container">
         {FILTER_ITEMS.map((item) => {
           return (
-            <div className="filter-item">
+            <div className={`filter-item ${item.id === selectFilter ? 'selected' : ''}`}
+              onClick={() => setSelectFilter(item.id)}
+            >
               <div className="filter-name">
                 <img src={item.iconPath} />
                 <p>{item.label}</p>
